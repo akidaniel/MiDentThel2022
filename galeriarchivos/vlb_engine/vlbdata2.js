e0cd1f9ebc6204a1f3d6838e2e72696b9ed8ae2a
Daniel <vivadaniel@hotmail.com>
2023-10-19 15:29:06 +0200

    updt

--- galeriarchivos/vlb_engine/vlbdata2.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:57,classNames:'vlightbox2', closeLocation:'top',descSliding:true,enableRightClick:false,enableSlideshow:true,overlayOpacity:0,prefix:'vlb1',resizeSpeed:9,slideTime:9,startZoom:true}) });
