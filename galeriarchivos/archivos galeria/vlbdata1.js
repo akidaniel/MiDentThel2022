d093d181abb8fb35527f5f3f3574bc403527d2ed
Daniel <vivadaniel@hotmail.com>
2023-10-24 10:46:57 +0200

    updt

---
+++ galeriarchivos/archivos galeria/vlbdata1.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:57,classNames:'vlightbox1', closeLocation:'top',descSliding:true,enableRightClick:false,enableSlideshow:true,overlayOpacity:0,prefix:'vlb1',resizeSpeed:9,slideTime:9,startZoom:true}) });
+
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:57,classNames:'vlightbox2', closeLocation:'top',descSliding:true,enableRightClick:false,enableSlideshow:true,overlayOpacity:0,prefix:'vlb1',resizeSpeed:9,slideTime:9,startZoom:true}) });
+
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:57,classNames:'vlightbox3', closeLocation:'top',descSliding:true,enableRightClick:false,enableSlideshow:true,overlayOpacity:0,prefix:'vlb1',resizeSpeed:9,slideTime:9,startZoom:true}) });
+
+
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:57,classNames:'vlightbox4', closeLocation:'top',descSliding:true,enableRightClick:false,enableSlideshow:true,overlayOpacity:0,prefix:'vlb1',resizeSpeed:9,slideTime:9,startZoom:true}) });
+
+
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:57,classNames:'vlightbox5', closeLocation:'top',descSliding:true,enableRightClick:false,enableSlideshow:true,overlayOpacity:0,prefix:'vlb1',resizeSpeed:9,slideTime:9,startZoom:true}) });
+
